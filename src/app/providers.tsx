33204f9fb9c15ee4405b0e7d1347ee7fc3ec7b42
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-09-27 12:28:11 -0700

    Fix urql capitalization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,13 @@
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 import {
-  Client as URQLClient,
-  Provider as URQLProvider,
+  Client as UrqlClient,
+  Provider as UrqlProvider,
   cacheExchange,
   fetchExchange,
 } from "urql";
 
-const urqlClient = new URQLClient({
+const urqlClient = new UrqlClient({
   url: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
   exchanges: [cacheExchange, fetchExchange],
 });
@@ -17,7 +17,7 @@ const urqlClient = new URQLClient({
 export function Providers(props: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class">
-      <URQLProvider value={urqlClient}>{props.children}</URQLProvider>
+      <UrqlProvider value={urqlClient}>{props.children}</UrqlProvider>
     </ThemeProvider>
   );
 }
