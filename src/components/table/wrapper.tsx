b1bc5e2d7f670d321383b8e05dc3b9e44aa4fe0f
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-09-29 15:57:55 -0700

    Remove margin from table wrapper

--- src/components/table/wrapper.tsx
+++ src/components/table/wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Wrapper(props: { children: ReactNode }) {
   return (
-    <div className="mt-2 overflow-x-auto rounded-lg border-2 border-gray-200 dark:border-gray-800">
+    <div className="overflow-x-auto rounded-lg border-2 border-gray-200 dark:border-gray-800">
       {props.children}
     </div>
   );
