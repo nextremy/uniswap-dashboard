77450341d95d649f0fef4683d4b65c3b74ada4f6
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-09-29 14:42:08 -0700

    Enlarge header font

--- src/app/_header/index.tsx
+++ src/app/_header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="flex h-16 items-center justify-between border-b border-gray-300 dark:border-gray-700">
       <Link
-        className="grid h-16 place-items-center px-4 text-lg font-medium"
+        className="grid h-16 place-items-center px-4 text-xl font-medium"
         href="/"
       >
         Uniswap Dashboard
