b01df1c3637fecab9c6bf35d26dddf5cfbe71c0f
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-09-29 16:59:00 -0700

    Create reusable icon button component

---
+++ src/components/icon-button/index.tsx
@@ -0,0 +1,13 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentPropsWithoutRef } from "react";
+
+const iconButton = cva(
+  "grid h-10 w-10 place-items-center rounded-md bg-gray-200 text-gray-700 duration-150 enabled:hover:bg-gray-300 disabled:opacity-50 dark:bg-gray-800 dark:text-gray-300 enabled:dark:hover:bg-gray-700",
+);
+
+type IconButtonProps = ComponentPropsWithoutRef<"button"> &
+  VariantProps<typeof iconButton>;
+
+export function IconButton({ className, ...props }: IconButtonProps) {
+  return <button className={iconButton({ className })} {...props} />;
+}
