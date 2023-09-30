354c3aaae4b69b234cec276fb829849f6caf5dcf
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-09-29 17:00:15 -0700

    Reduce header border contrast

--- src/app/_header/index.tsx
+++ src/app/_header/index.tsx
@@ -3,7 +3,7 @@ import { ThemeButton } from "./theme-button";
 
 export function Header() {
   return (
-    <div className="border-b-2 border-gray-300 dark:border-gray-700">
+    <div className="border-b-2 border-gray-200 dark:border-gray-800">
       <header className="mx-auto flex h-16 max-w-7xl items-center justify-between">
         <Link
           className="grid h-16 place-items-center px-4 text-xl font-medium"
